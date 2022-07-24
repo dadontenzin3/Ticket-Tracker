80a6d802dae88d3f9a4fe4aff07b069baa960ae9
dadontenzin3 <dadontenzin1@gmail.com>
2022-07-24 17:16:42 -0400

    connect to mongoDB

--- server.js
+++ server.js
@@ -1,18 +1,28 @@
 // dependencies 
+require('dotenv').config();
 const express = require('express');
 const PORT = 3000;
-
+const ticketsRouter = require('./controllers/tickets');
+const mongoose = require('mongoose');
 
 // initialize express app 
 const app = express();
 
 // configure application setup 
+const DATABASEURL = process.env.DATABASE_URL;
 
+mongoose.connect(DATABASEURL);
+
+mongoose.connection.on('connected', () => {
+    console.log('Connected to MongoDB');
+})
 
 // mount route 
 app.get('/', (req,res) =>{
     res.send('listening')
-})
+});
+
+app.use('/', ticketsRouter);
 
 // port listener 
 app.listen(PORT, () =>{
