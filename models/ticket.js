6ba9bd95793f51b0cc25a41af15270066af2f1a2
dadontenzin3 <dadontenzin1@gmail.com>
2022-07-24 16:16:43 -0400

    define ticket model

---
+++ models/ticket.js
@@ -0,0 +1,30 @@
+// require dependencies 
+const mongoose = require('mongoose');
+
+//initialize a shortcut variable 
+const Schema = mongoose.Schema;
+
+// define the schema 
+const ticketSchema = new Schema({
+    ticket: {
+        type: Number,
+        require: true
+    },
+    department: {
+        type: String,
+        require: true
+    },
+    request: {
+        type: String, 
+        require: true
+    },
+    status: {
+        type: String, 
+        require: true
+    }   
+}, {timestamps: true});
+
+// export the model to be used within our controller 
+const Ticket = mongoose.model('Ticket', ticketSchema);
+
+module.exports = Ticket; 
